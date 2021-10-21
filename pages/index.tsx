0ad6d35a1f4872e775bab70341897d7ec288890d
sajad saderi <sajad.saderi@onoffapp.com>
2021-10-21 10:12:40 +0330

    accessing the folders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { Dropbox } from 'dropbox';
+
+const token =
+  'sl.A6znWBeiSThYrN3_sHS4yVJX2wGIHJZlxjq2hhH8AiKQGKxGwXV70vhwJueCDCculOhmbaQmNTH48UIxG3Hke7x19Gpn9onT-tL2SVKBNtH8SvOcQZSU-YZNxOvToCq8DKa4uQu34KFk';
 
 const Home: NextPage = () => {
+  const [listOfQueries, setListOfQueries] = useState<any>();
+  useEffect(() => {
+    gettingDropboxInfo();
+  }, []);
+
+  const gettingDropboxInfo = async () => {
+    const dropBox = new Dropbox({ accessToken: token });
+
+    const res = await dropBox.filesListFolder({ path: '/Otoli Document' });
+    setListOfQueries(res.result.entries);
+    console.log('dropBoxInfo', res);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +31,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {listOfQueries &&
+          listOfQueries.map((entry: any) => <p key={entry.id}>{entry.name}</p>)}
       </main>
 
       <footer className={styles.footer}>
@@ -66,7 +48,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
